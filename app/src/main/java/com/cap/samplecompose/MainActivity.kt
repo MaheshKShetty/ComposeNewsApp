135b5929c840ba169ba8e37e9470b5588cfb9d7c
MaheshKShetty <maheshkumarshetty1@gmail.com>
2023-08-29 19:17:25 +0530

    Feat -> news screen issue fix

--- app/src/main/java/com/cap/samplecompose/MainActivity.kt
+++ app/src/main/java/com/cap/samplecompose/MainActivity.kt
@@ -1,36 +1,13 @@
 package com.cap.samplecompose
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.cap.samplecompose.model.Resource
 import com.cap.samplecompose.network.NetworkViewModel
-import com.cap.samplecompose.news.NewsScreen
 import com.cap.samplecompose.splash.SplashViewModel
 import com.cap.samplecompose.ui.theme.MyApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -48,7 +25,7 @@ class MainActivity : ComponentActivity() {
         networkViewModel.getNewList()
         setContent {
             MyApplicationTheme {
-                val navController:NavHostController= rememberNavController()
+                val navController: NavHostController = rememberNavController()
                 Navigation(networkViewModel, navController = navController)
                 navController.navigate(NewsScreenRoute.route)
             }
